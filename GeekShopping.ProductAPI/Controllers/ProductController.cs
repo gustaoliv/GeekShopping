af5d0243d26b7ba1954ea9dd3e0d52e167fdad4f
Gustavo Oliveira <gusta.oliv4@gmail.com>
2023-02-01 22:33:57 -0300

    [GeekShopping.ProductAPI] Implementation of FindAll and FindById routes

--- GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.ProductAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            IEnumerable<ProductVO> productsVO = await _repository.FindAll();
+            return Ok(productsVO);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            ProductVO productVO  = await _repository.FindById(id);
+            if (productVO == null)
+                return NotFound();
+            else
+                return Ok(productVO);
+        }
     }
 }
