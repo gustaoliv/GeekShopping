756b384deafbbcff8af33b6d84a07c583b74f252
Gustavo Oliveira <gusta.oliv4@gmail.com>
2023-02-01 22:13:09 -0300

    [GeekShopping.ProductAPI] Add ProductRepository dependence injection

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -17,6 +18,9 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
 IMapper mapper = MappingConfig.RegiterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
