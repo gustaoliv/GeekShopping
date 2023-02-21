7d8ab93903f4cd23f9399e406b33e6ba814b8898
Gustavo Oliveira <gusta.oliv4@gmail.com>
2023-02-21 12:08:13 -0300

    [GeekShopping.IdentityServer] Create project and add db context

--- GeekShopping.IdentityServer/Models/ErrorViewModel.cs
+++ GeekShopping.IdentityServer/Models/ErrorViewModel.cs
@@ -1,8 +1,8 @@
-namespace GeekShopping.Web.Models
+namespace GeekShopping.IdentityServer.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
