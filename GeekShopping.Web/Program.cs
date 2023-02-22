a251c242476135772d663cc7b5b34f2c99b28ab6
Gustavo Oliveira <gusta.oliv4@gmail.com>
2023-02-22 13:47:47 -0300

    [GeekShopping.IdentityServer] Add authentication to swagger

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,5 +1,6 @@
 using GeekShopping.Web.Services.IServices;
 using GeekShopping.Web.Services;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,29 @@ builder.Services.AddRazorPages()
 builder.Services.AddHttpClient<IProductService, ProductService>(c =>
     c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
 );
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+
+}).AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+  .AddOpenIdConnect("oidc", options =>
+  {
+      options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+      options.GetClaimsFromUserInfoEndpoint = true;
+      options.ClientId = "geek_shopping";
+      options.ClientSecret = "my_super_secret";
+      options.ResponseType = "code";
+      options.ClaimActions.MapJsonKey("role", "role", "role");
+      options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+      options.TokenValidationParameters.NameClaimType = "name";
+      options.TokenValidationParameters.RoleClaimType = "role";
+      options.Scope.Add("geek_shopping");
+      options.SaveTokens = true;
+  }
+);
+
 
 var app = builder.Build();
 
@@ -28,6 +52,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
