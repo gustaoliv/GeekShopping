a93da5391f81bbe74262fb96cd5b1c50a856f4a4
Gustavo Oliveira <gusta.oliv4@gmail.com>
2023-02-22 14:32:12 -0300

    [GeekShopping.IdentityServer] Implementation of new users registration

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddControllers();
 
 
 builder.Services.AddAuthentication("Bearer")
-                .AddJwtBearer(options => {
+                .AddJwtBearer("Bearer", options => {
                     options.Authority = "https://localhost:4435/";
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
