6c9138906ebc44906c1d29145acaea378bb4d2cc
Gustavo Oliveira <gusta.oliv4@gmail.com>
2023-03-12 22:44:07 -0300

    [GeekShopping.CouponAPI] Create context, controller and seed data

--- GeekShopping.CouponAPI/Model/Context/MySQLContext.cs
+++ GeekShopping.CouponAPI/Model/Context/MySQLContext.cs
@@ -9,5 +9,29 @@ namespace GeekShopping.CouponAPI.Model.Context
 
         public DbSet<Coupon> Coupons { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon()
+                {
+                    Id = 1,
+                    CouponCode = "GUSTAVO_10",
+                    DiscountAmount = 10,
+                }
+            );
+
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon()
+                {
+                    Id = 2,
+                    CouponCode = "GUSTAVO_20",
+                    DiscountAmount = 20,
+                }
+            );
+
+
+        }
     }
 }
